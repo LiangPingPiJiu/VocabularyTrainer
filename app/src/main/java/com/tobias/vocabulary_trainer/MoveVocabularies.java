a8c05e13bde977b2f81a395b2b5b17430f782367
LiangPingPiJiu <tobias.endrikat@posteo.de>
2016-11-20 01:19:00 +0100

    added functionality to move selected vocabularies to another box! :)

---
+++ app/src/main/java/com/tobias/vocabulary_trainer/MoveVocabularies.java
@@ -0,0 +1,13 @@
+package com.tobias.vocabulary_trainer;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MoveVocabularies extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.move_vocabularies_layout);
+    }
+}
